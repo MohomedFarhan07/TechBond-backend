adbfd7c5c9589ac4bc5e25beb0d1da95dd0ae518
MohomedFarhan07 <offxl.mohofarhan07@gmail.com>
2026-02-24 17:19:48 +0530

    feat: implemented chat feature using socket.io

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const connectDB = require("../config/database");
+const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http");
+const initializeSocket = require("./utils/socket.js");
 
 app.use(
   cors({
@@ -13,20 +15,23 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-const authRouter = require("../routes/auth");
-const profileRouter = require("../routes/profile");
-const requestRouter = require("../routes/request");
-const userRouter = require("../routes/user");
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/profile");
+const requestRouter = require("./routes/request");
+const userRouter = require("./routes/user");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
 
+const server = http.createServer(app);
+initializeSocket(server);
+
 connectDB()
   .then(() => {
     console.log("Database connection is established...");
-    app.listen(3000, () => {
+    server.listen(3000, () => {
       console.log("Successfully listening to the port 3000...");
     });
   })
