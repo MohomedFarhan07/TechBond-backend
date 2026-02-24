fb8e14c1dc39363e12681f37e505574cd9e48f51
MohomedFarhan07 <offxl.mohofarhan07@gmail.com>
2026-02-24 20:30:44 +0530

    feat: created a schema to store messages

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,6 @@
 const socket = require("socket.io");
-const crypto = require('crypto');
+const crypto = require("crypto");
+const Chat = require("../models/chat");
 
 const getSecretRoomId = (userId, targetUserId) => {
   return crypto
@@ -18,13 +19,36 @@ const initializeSocket = (server) => {
   io.on("connection", (socket) => {
     socket.on("joinChat", ({ userId, targetUserId }) => {
       const roomId = getSecretRoomId(userId, targetUserId);
-      socket.join(roomId); 
+      socket.join(roomId);
     });
 
-    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
-      const roomId = getSecretRoomId(userId, targetUserId);
-      io.to(roomId).emit("messageReceived", { firstName, text });
-    });
+    socket.on(
+      "sendMessage",
+      async ({ firstName, userId, targetUserId, text }) => {
+        const roomId = getSecretRoomId(userId, targetUserId);
+
+        try {
+          let chat = await Chat.findOne({
+            participants: { $all: [userId, targetUserId] },
+          });
+          if (!chat) {
+            chat = new Chat({
+              participants: [userId, targetUserId],
+              messages: [],
+            });
+          }
+
+          chat.messages.push({
+            senderId: userId,
+            text,
+          });
+          await chat.save();
+          io.to(roomId).emit("messageReceived", { firstName, text });
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    );
 
     socket.on("disconnect", () => {});
   });
