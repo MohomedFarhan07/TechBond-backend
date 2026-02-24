12792ff019a419653773d16065d6cd1343fed798
MohomedFarhan07 <offxl.mohofarhan07@gmail.com>
2026-02-24 12:44:53 +0530

    fix: added new api folder

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const connectDB = require("./config/database");
+const connectDB = require("../config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -13,10 +13,10 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-const authRouter = require("./routes/auth");
-const profileRouter = require("./routes/profile");
-const requestRouter = require("./routes/request");
-const userRouter = require("./routes/user");
+const authRouter = require("../routes/auth");
+const profileRouter = require("../routes/profile");
+const requestRouter = require("../routes/request");
+const userRouter = require("../routes/user");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
